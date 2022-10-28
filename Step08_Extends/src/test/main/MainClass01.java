3ce3ea3c622069c0e057d5e12a7f1bebc16e2dd4
WOO-BIN LEE <iopplm89@gmail.com>
2022-10-28 09:36:55 +0900

    Merge branch 'master' of https://github.com/Wkong89/First_Eclipse_java.git

--- Step08_Extends/src/test/main/MainClass01.java
+++ Step08_Extends/src/test/main/MainClass01.java
@@ -5,7 +5,7 @@ import test.mypac.HandPhone;
 public class MainClass01 {
 	public static void main(String[] args) {
 		/*
-		 * phone 클래스를 상속받은 HandPhone 클래스로 객체를 생성해서
+		 * Phone 클래스를 상속받은 HandPhone 클래스로 객체를 생성해서
 		 * 그 참조값을 p1 이라는 지역변수에 담기
 		 */
 		HandPhone p1=new HandPhone();
