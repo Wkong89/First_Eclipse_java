e79bbd465ad4d3a0add2d1662904b90f83d73f86
WOO-BIN LEE <iopplm89@gmail.com>
2022-10-31 18:10:21 +0900

    Step12_UtilClass (QuizMain) 클래스 추가함

--- Step12_UtilClass/src/test/main/QuizMain.java
+++ Step12_UtilClass/src/test/main/QuizMain.java
@@ -1,8 +1,32 @@
 package test.main;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class QuizMain {
 	public static void main(String[] args) {
 		// sample 데이터
+		Map<String, String> dic= new HashMap<>();
+		dic.put("house", "집");
+		dic.put("phone", "전화기");
+		dic.put("car", "자동차");
+		dic.put("pencil", "연필");
+		dic.put("eraser", "지우개");
+		dic.put("computer", "컴퓨터");
+		
+		/*
+		 * 		검색할 단어를 입력하세요:house
+		 * 		house 의 뜻은 집 입니다.
+		 * 
+		 * 		검색할 단어를 입력하세요:gura
+		 * 		gura 는 목록에 없습니다.
+		 */
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("검색할 단어를 입력하세요: ");
+		String s1=scan.nextLine();
+		System.out.println(s1 +" 의 뜻은 "+dic.get(s1)+" 입니다.");
 		
 	}
 }
